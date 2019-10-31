a5dba72479cffcea428025929234114de64366dd
braian <braian.martino@gmail.com>
2019-10-30 21:47:33 -0300

    Subo cambios 30/10

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require ("mongoose");
+const Citie = require("./citie");
+const cors = require("cors");
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.json("hola").status(200);
+});
+
+app.get("/cities",(req,res)=>{
+    mongoose.connect('mongodb+srv://braian:mirta1989@mytinerary-xyiit.gcp.mongodb.net/test?retryWrites=true&w=majority')
+
+    .then(()=>{
+        Citie
+            .find({}).then((citiesFinded)=>{res.json(citiesFinded).status(204)})
+    })
+   
+})
+
+app.listen(8080,()=>{console.log("server is up")})
+
